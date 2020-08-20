07e37427207ecddeec04954bb06617397c702a15
Hari Amoor <amoor.hari@gmail.com>
2020-08-20 19:39:22 -0400

    Added some if-statements

--- config.fish
+++ config.fish
@@ -3,6 +3,10 @@ if test (which emacs)
     set -x VISUAL "emacsclient -c -a emacs"
 end
 
+if test (which firefox)
+    set -x BROWSER (which firefox)
+end
+
 if test -d /usr/local/go
     # Set up Golang config, if installed
     # with binary
@@ -10,13 +14,24 @@ if test -d /usr/local/go
     set -x GOROOT "/usr/local/go"
     set -x GOPATH (go env GOPATH)
 
-    set -gx PATH $GOROOT/bin $GOPATH/bin $PATH
+    begin
+        string match "*$GOROOT/bin*" $PATH
+        or string match "*$GOPATH/bin*" $PATH
+    end
+    or begin
+        set -gx PATH $GOROOT/bin $GOPATH/bin $PATH
+    end
 end
 
 if test -d $HOME/.cargo
-    # Set up Rust config, if installed
-    # with Rustup, i.e. from Rustup script
-    set -gx PATH $HOME/.cargo/bin $PATH
+    begin
+        string match "*$HOME/.cargo/bin*" $PATH
+    end
+    or begin
+        # Set up Rust config, if installed
+        # with Rustup, i.e. from Rustup script
+        set -gx PATH $HOME/.cargo/bin $PATH
+    end
 end
 
 if test (uname -s) = Darwin
